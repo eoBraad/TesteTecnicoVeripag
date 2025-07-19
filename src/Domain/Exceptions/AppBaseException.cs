2dc036755e0d18c560cd336dcd5c9d09af19466c
EoBread <joao.alencar099@gmail.com>
2025-07-19 13:02:04 -0300

    Add custom exception classes for error handling

---
+++ src/Domain/Exceptions/AppBaseException.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Exceptions;
+
+public abstract class AppBaseException(string message) : SystemException(message)
+{
+    public abstract int StatusCode { get; }
+    public abstract List<string> GetErrors();
+}
---
+++ src/Domain/Exceptions/AppValidationException.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace Domain.Exceptions;
+
+public class AppValidationException(List<string> errors) : AppBaseException(string.Empty)
+{
+    private readonly List<string> _errors = errors;
+
+    public override int StatusCode => (int)HttpStatusCode.BadRequest;
+
+    public override List<string> GetErrors()
+    {
+        return _errors;
+    }
+}
