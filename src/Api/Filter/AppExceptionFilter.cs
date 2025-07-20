93a627900da769f11703ae59ffbb114501ad4122
EoBread <joao.alencar099@gmail.com>
2025-07-20 13:39:42 -0300

    Enhance AppExceptionFilter to log errors with detailed log message structure

--- src/Api/Filter/AppExceptionFilter.cs
+++ src/Api/Filter/AppExceptionFilter.cs
@@ -1,12 +1,14 @@
 ﻿using Domain.Exceptions;
+using Domain.Messages;
 using Domain.Responses;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Api.Filter;
 
-public class AppExceptionFilter : IExceptionFilter
+public abstract class AppExceptionFilter(ILogger<AppExceptionFilter> logger) : IExceptionFilter
 {
+    private readonly ILogger<AppExceptionFilter> _logger = logger;
     public void OnException(ExceptionContext context)
     {
         if (context.Exception is AppBaseException)
@@ -23,6 +25,9 @@ public class AppExceptionFilter : IExceptionFilter
     {
         var appBaseException = (AppBaseException)context.Exception;
         var errorResponse = new ResponseErrorJson(appBaseException.GetErrors());
+        
+        var logMessage = new LogMessage(appBaseException.GetErrors(), context.HttpContext.Request.Path);
+        _logger.LogError($"Ocorreu um erro: {logMessage}");
 
         context.HttpContext.Response.StatusCode = appBaseException.StatusCode;
         context.Result = new ObjectResult(errorResponse);
---
+++ src/Domain/Messages/LogMessage.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Messages;
+
+public class LogMessage
+{
+    public List<string> Messages { get; set; }
+    
+    public DateTime Date { get; set; } = DateTime.Now;
+    
+    public string Path { get; set; }
+    
+    public LogMessage(List<string> messages, string path)
+    {
+        Messages = messages;
+        Path = path;
+    }
+}
